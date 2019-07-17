1695928b8cfde907c83ec5998b0af7bfd037677c
Vadim Smirnov <cocolacre@gmail.com>
2019-07-17 21:40:49 +0300

    Create runtime_compile_example.cs

---
+++ runtime_compile_example.cs
@@ -0,0 +1,13 @@
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+public class CompilerTest {
+	public object RunCode (string userCode) {
+	CSharpCodeProvider provider = new CSharpCodeProvider ();
+	CompilerResults results = provider.CompileAssemblyFromSource (new CompileParameters (), userCode);
+	System.Type classType = results.CompiledAssembly.GetType("MyClass");
+	System.Reflection.MethodInfo method = classType.GetMethod("MyMethod");
+	
+	return method.Invoke (null, null);
+	}
+}
