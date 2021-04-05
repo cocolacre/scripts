5cef8ec9eeb3883b5b6082ecb842886bccde17d9
cocolacre <cocolacre@gmail.com>
2021-04-05 21:57:38 +0300

    added keyrate script

--- doskeys.bat
+++ doskeys.bat
@@ -1,48 +1,42 @@
 @echo off
+color 0a
 ::chcp 65001
-
+echo UPDATE DOSKEYS.BAT!
 ::actual doskeys
-
+::echo "PATH TO USER DIRECTORY:" %USERPROFILE%
 ::ld = load doskeys
+doskey ld="%USERPROFILE%\scripts\doskeys.bat"
+doskey dsks="C:\Program Files\Notepad++\notepad++.exe" %USERPROFILE%\scripts\doskeys.bat
 doskey colors=type C:\Users\2\Documents\colors.txt
 doskey blue=color 09
 doskey sky=color 03
 doskey green=color 0a
-doskey ld=C:\users\2\doskeys.bat
-doskey p2=C:\Python27\python.exe -i $*
-doskey p264=C:\Python27-64\python.exe -i $*
-doskey p73=C:\Python37-32\python.exe -i $*
-doskey p37=C:\Python37-64\python.exe -i $*
-doskey npp="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
+doskey npp="C:\Program Files\Notepad++\notepad++.exe" $*
 doskey qq=exit
-doskey dsks="C:\Program Files (x86)\Notepad++\notepad++.exe" C:\users\2\doskeys.bat
-doskey docs=cd C:\users\user\Documents
-doskey home=cd C:\users\user
-doskey pip2=C:\Python27\Scripts\pip.exe $*
+doskey docs=cd %USERPROFILE%\Documents
+doskey home=cd %USERPROFILE%
 doskey vlc0=taskkill /f /im vlc.exe
-doskey yt=C:\Python27\Scripts\youtube-dl.exe $*
-doskey kr=D:\pi17\keyrate.cmd $*
+doskey yt=C:\Python39\Scripts\youtube-dl.exe $*
+doskey kr=%USERPROFILE%\scripts\keyrate\keyrate.cmd $*
 :: 140 35
 ::doskey ffmpeg=C:\Windows\System32\ffmpeg.exe $*
-doskey fmpeg=C:\Users\2\ffmpeg.exe $*
-doskey cr="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
-doskey vlc1="C:\Program Files\VideoLAN\VLC\vlc.exe" $*
-doskey ffplay=C:\Windows\System32\ffplay.exe $*
-doskey v=taskkill /f /im vlc.exe
-doskey putty=C:\PuTTY\putty.exe $*
-doskey sftp=C:\PuTTY\psftp.exe $*
-doskey plink=C:\PuTTY\plink.exe $*
-doskey mcssh=C:\PuTTY\putty.exe -i D:\pi23_MCS_server\1.ppk centos@95.163.215.149
+::doskey fmpeg=C:\Users\2\ffmpeg.exe $*
+doskey cr="C:\Program Files\Google\Chrome\Application\chrome.exe" $*
+::doskey vlc1="C:\Program Files\VideoLAN\VLC\vlc.exe" $*
+::doskey ffplay=C:\Windows\System32\ffplay.exe $*
+::doskey putty=C:\PuTTY\putty.exe $*
+::doskey sftp=C:\PuTTY\psftp.exe $*
+::doskey plink=C:\PuTTY\plink.exe $*
+::doskey mcssh=C:\PuTTY\putty.exe -i D:\pi23_MCS_server\1.ppk centos@95.163.215.149
 
-doskey jn=C:\Python27\Scripts\jupyter-notebook.exe
+::doskey jn=C:\Python27\Scripts\jupyter-notebook.exe
 doskey sh=C:\cygwin64\Cygwin.bat $*
-doskey yd=C:\Python27\Scripts\youtube-dl.exe $*
 doskey sq=C:\sqlite\sqlite3.exe $*
 doskey procexp="C:\Users\2\Downloads\ProcessExplorer\procexp.exe"
-doskey p3=C:\Python36-64\python.exe -i $*
-doskey fixtab=C:\Python27\python.exe C:\Users\2\Documents\tab2spaces.py $*
-doskey jn=C:\Python36-64\Scripts\jupyter-notebook.exe $*
-doskey SqlLocalDB="C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" $*
+doskey p3=C:\Python39\python.exe -i $*
+::doskey fixtab=C:\Python27\python.exe C:\Users\2\Documents\tab2spaces.py $*
+::doskey jn=C:\Python36-64\Scripts\jupyter-notebook.exe $*
+::doskey SqlLocalDB="C:\Program Files\Microsoft SQL Server\130\Tools\Binn\SqlLocalDB.exe" $*
 ::##################################################################################
 
 
@@ -58,38 +52,39 @@ doskey rec10_2va=ffmpeg -rtbufsize 1024M -f gdigrab -offset_x 1366 -offset_y 0 -
 doskey rec10_2v=ffmpeg -rtbufsize 1024M -f gdigrab -offset_x 1366 -offset_y 0 -video_size 1920x1080 -framerate 10 -i desktop $1.mp4
 
 ::doskey shcam=ffmpeg -f dshow -framerate 15 -video_size 320x240 -i video="Integrated Camera" -pix_fmt yuv420p -f mpegts - | ffplay -analyzeduration 10 -f mpegts -
-doskey shcam=C:\Users\2\Documents\Task-Stack-Widget\task-data\1363\solution.cmd
+::doskey shcam=C:\Users\2\Documents\Task-Stack-Widget\task-data\1363\solution.cmd
 ::##################################################################################
 
-doskey SQLCMD110="C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SQLCMD.exe" $*
-doskey bcp110="C:\Program Files\Microsoft SQL Server\110\Tools\Binn\bcp.exe" $*
-doskey dumpbin64="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\dumpbin.exe" $*
-doskey dumpbin32="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx86\x86\dumpbin.exe" $*
-doskey chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
-doskey sqlite="C:\Users\2\Documents\sqlite\sqlite3.exe" $*
-doskey wsldir=cd "C:\Users\2\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs"
-doskey copy_wsldir =echo "C:\Users\2\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs" | clip
-doskey junction="C:\Users\2\Documents\Scripts\Junction\junction.exe"
+::doskey SQLCMD110="C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SQLCMD.exe" $*
+::doskey bcp110="C:\Program Files\Microsoft SQL Server\110\Tools\Binn\bcp.exe" $*
+::doskey dumpbin64="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\dumpbin.exe" $*
+::doskey dumpbin32="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx86\x86\dumpbin.exe" $*
+::doskey chrome="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
+doskey sqlite="C:\Users\%USERPROFILE%\Documents\sqlite\sqlite3.exe" $*
+::doskey wsldir=cd "C:\Users\2\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs"
+::doskey copy_wsldir =echo "C:\Users\2\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs" | clip
+::doskey junction="C:\Users\2\Documents\Scripts\Junction\junction.exe"
 doskey exp=explorer %CD% $*
 :: Notice "%%h" instead of "%h" AND "%%x0D" in the end - a fix for batch script.
 :: QUICK CD NAVIGATE SHORTCUTS.
-doskey f1=cd C:\Users\2\Documents\Task-Stack-Widget\ 
-doskey f2=cd C:\Users\2\Documents\Task-Stack-Widget\task-data\
-doskey f3=cd C:\
-doskey f4=cd C:\Windows\Microsoft.NET
-doskey f5=cd C:\Users\2\Documents\Task-Stack-Widget\task-data\1206
+::doskey f1=cd C:\Users\2\Documents\Task-Stack-Widget\ 
+::doskey f2=cd C:\Users\2\Documents\Task-Stack-Widget\task-data\
+::doskey f3=cd C:\
+::doskey f4=cd C:\Windows\Microsoft.NET
+::doskey f5=cd C:\Users\2\Documents\Task-Stack-Widget\task-data\1206
 doskey reboot=shutdown /f /r
 doskey sleep=rundll32.exe powrprof.dll,SetSuspendState 0,1,0
 
 ::git
-doskey gpom=git push origin master $*
 doskey giche=git checkout $*
 doskey gcm=git commit -m $*
 doskey undo="C:\Users\2\Documents\git-tutorial\git-undo.bat"
 doskey ga=git add -A
 doskey git-bash="C:\Program Files\Git\git-bash.exe"
-doskey gpu=git push origin master
-doskey gpull=git pull origin master
+doskey gpu=git push origin main
+doskey gpuster=git push origin master
+doskey gpull=git pull origin main
+doskey gpullster=git pull origin master
 ::doskey gitlog=git log --date=format:"%%Y-%%m-%%d %%H:%%M" --pretty=format:"%%h%%x09%%ai%%x09%%Cblue%%s%%x0D"
 doskey gitlog=git log --pretty=format:"%%h%%x09%%ai%%x09%%Cblue%%s%%x0D"
 doskey githelp="C:\Program Files\Git\mingw64\share\doc\git-doc\git.html"
@@ -134,21 +129,21 @@ doskey vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\
 doskey expcd=explorer %%cd%%
 
 ::gcloud
-doskey gcl="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" $*
-doskey gstop="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances stop instance-5
-doskey gstop2="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances stop instance-6
-doskey gstart="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances start instance-5
-doskey gstart2="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances start instance-6
-doskey gcloud="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" $*
-doskey centos="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" beta compute --project "mystic-song-226805" ssh --zone "us-central1-a" "instance-6"
-doskey winserver="C:\Windows\System32\mstsc.exe" C:\Users\2\Documents\Task-Stack-Widget\task-data\1032\instance-5.rdp
-doskey ws="C:\Windows\System32\mstsc.exe" C:\Users\2\Documents\Task-Stack-Widget\task-data\1032\instance-5.rdp
+::doskey gcl="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" $*
+::doskey gstop="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances stop instance-5
+::doskey gstop2="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances stop instance-6
+::doskey gstart="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances start instance-5
+::doskey gstart2="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" compute instances start instance-6
+::doskey gcloud="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" $*
+::doskey centos="C:\Users\2\AppData\Local\Google\Cloud SDK\google-cloud-sdk.staging.staging\bin\gcloud.cmd" beta compute --project "mystic-song-226805" ssh --zone "us-central1-a" "instance-6"
+::doskey winserver="C:\Windows\System32\mstsc.exe" C:\Users\2\Documents\Task-Stack-Widget\task-data\1032\instance-5.rdp
+::doskey ws="C:\Windows\System32\mstsc.exe" C:\Users\2\Documents\Task-Stack-Widget\task-data\1032\instance-5.rdp
 
-doskey qtdemo=C:\Python36-64\python.exe C:\Python36-64\Lib\site-packages\PyQt4\examples\demos\qtdemo\qtdemo.pyw
+::doskey qtdemo=C:\Python36-64\python.exe C:\Python36-64\Lib\site-packages\PyQt4\examples\demos\qtdemo\qtdemo.pyw
 
 :: LINUX TOOLS
 doskey grep=C:\cygwin64\bin\grep.exe $*
 
 ::PYTHON TOOLS
-doskey p2help="C:\Python36-64\Doc\python365.chm"
-doskey p3help="C:\Python27-64\Doc\python2715.chm"
+::doskey p2help="C:\Python36-64\Doc\python365.chm"
+doskey p3help=C:\Python39\Doc\python392.chm
---
+++ keyrate/keyrate.cmd
@@ -0,0 +1,6 @@
+echo ADJUST Keyrate - make a training-evaluating script to optimize text editing
+:: criteria  - low param (coding engagement)
+:: criteria high param (amount of bacckspaces pressed? coontinuity of input?)
+:: probllem example? manually filter heavily-tagged html pagge 
+::https://goo.gl/mhACZY
+@powershell -NoProfile -ExecutionPolicy Unrestricted "C:\Users\79951\scripts\keyrate\keyrate.ps1" %*
