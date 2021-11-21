ddea6ddc109bb4076d8d4dfcdbf9577985dd7530
Vadim Smirnov <cocolacre@gmail.com>
2021-11-21 12:03:23 +0300

    truly multiplatform now

--- measure-physiology-reaction-v1.py
+++ measure-physiology-reaction-v1.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtCore import pyqtSignal
 import keyboard
 import time, random, os
+import signal
 
 class Window(QWidget):
     """
@@ -78,38 +79,41 @@ class Window(QWidget):
         if self.wait_for_keypress:
             event = args[0]
             if event.event_type == "down":
+                # keypress done!
                 self.measurements[-1]["key_pressed_at"] = time.time()
                 self.measurements[-1]["reaction_time"] = self.measurements[-1]["key_pressed_at"] - self.measurements[-1]["shown_at"]
                 self.wait_for_keypress = False 
-                # keypress done!
-                self.counter+=1
-                self.new_measurement()
                 
-        if self.counter > self.num_measurements:
-            print("Unhooking ... ")
-            keyboard.unhook_all()
-            keyboard.unhook_all_hotkeys()
-            num_files = len(os.listdir())
-            
-            #print(self.measurements[:-1])
-            reaction_times = [measurement['reaction_time'] for measurement in self.measurements[:-1]]
-            reaction_time_min = min(reaction_times)
-            reaction_time_max = max(reaction_times)
-            reaction_time_avg = sum(reaction_times)/len(self.measurements[:-1])
-            
-            with open("measurement_" + str(num_files+1) + ".txt", "a+") as report:
-                report.write(time.ctime() + "\n")
-                report.write(self.signal_info + "\n")
-                report.write("reaction_time_min:\t" + str(reaction_time_min) + "\n")
-                report.write("reaction_time_max:\t" + str(reaction_time_max) + "\n")
-                report.write("reaction_time_avg:\t" + str(reaction_time_avg) + "\n")
-                report.write("### DATA: ###\n")
-                for item in self.measurements[:-1]: #drop last record
-                    for k,v in item.items():
-                        report.write(str(k) + "\t" + str(v))
-                        report.write("\n")
-                    report.write("\n")
-                report.write("#"*10 + "\n")
+                self.counter+=1
+                
+                if self.counter > self.num_measurements: #finish if counter exceeded planned measurements amount
+                    print("Unhooking ... ")
+                    keyboard.unhook_all()
+                    keyboard.unhook_all_hotkeys()
+                    num_files = len(os.listdir())
+                    
+                    #print(self.measurements[:-1])
+                    reaction_times = [measurement['reaction_time'] for measurement in self.measurements[:-1]]
+                    reaction_time_min = min(reaction_times)
+                    reaction_time_max = max(reaction_times)
+                    reaction_time_avg = sum(reaction_times)/len(self.measurements[:-1])
+                    
+                    with open("measurement_" + str(num_files+1) + ".txt", "a+") as report:
+                        report.write(time.ctime() + "\n")
+                        report.write(self.signal_info + "\n")
+                        report.write("reaction_time_min:\t" + str(reaction_time_min) + "\n")
+                        report.write("reaction_time_max:\t" + str(reaction_time_max) + "\n")
+                        report.write("reaction_time_avg:\t" + str(reaction_time_avg) + "\n")
+                        report.write("### DATA: ###\n")
+                        for item in self.measurements[:-1]: #drop last record
+                            for k,v in item.items():
+                                report.write(str(k) + "\t" + str(v))
+                                report.write("\n")
+                            report.write("\n")
+                        report.write("#"*10 + "\n")
+                    self.close()
+                else:
+                    self.new_measurement()
 
 if __name__ == "__main__":
     pid = os.getpid()
@@ -117,9 +121,10 @@ if __name__ == "__main__":
     window = Window()
     window.show()
     try:
-        pass
+        sys.exit(app.exec_())
+        os.kill(os.getpid(), signal.SIGTERM)
+        #pass
         #os.system('taskkill /f /PID %d'%pid)
     except Exception as _e: #if we are on Linux for example...
-        sys.exit(app.exec_())
+        pass
         
-        print("Kill process yourself you linuxoid user!")
